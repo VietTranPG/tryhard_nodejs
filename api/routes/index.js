0199f104c7d67165362fe39f2606330b16331cc7
viet <viet.tranhoang@powergatesoftware.com>
2017-07-12 22:25:17 +0700

    123

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.json({'test':'aaaaaaaaaaaa'});
 });
+router.post('/signup', function(req, res, next) {
+  console.log(res)
+});
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
+var config = require("config");
+var user_model = require("../model/user_model")
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-
+router.post('/signup', function (req, res, next) {
+  
+  var user = req.body;
+  var email = user.email;
+  var password = user.password;
+  if (email && password) {
+    console.log(req.body)
+    user_model.CreateUser(user)
+      .then(function (result) {
+        console.result;
+      }).error(function(error){
+        console.log(error);
+      });
+  } else {
+    res.json({
+      Data: [],
+      Message: "Email and Password is required!",
+      Status: "error"
+    });
+  }
+});
 module.exports = router;
